c43cd35f35ec33b33d6a66fcdf6ab34e9585239d
Michael Rockstein <mike.rockstein@web.de>
2011-11-27 22:37:37 +0100

    test

--- src/de/fhswf/verwaltung/Database.java
+++ src/de/fhswf/verwaltung/Database.java
@@ -1,5 +1,13 @@
 package de.fhswf.verwaltung;
 
+import java.io.File;
+
+import de.fhswf.db.Testdb;
+
 public class Database {
+	
+	private static final String DataBaseFileName = "fuhrpark_hsql_db";
+	private static final File DataBaseFile = new File(DataBaseFileName+".script");
+	private Testdb dataBase;
 
 }
