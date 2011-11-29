d60eb8caa7e3990b0f02ef736b7df63ddba67381
Michael Rockstein <mike.rockstein@web.de>
2011-11-29 19:53:07 +0100

    Edit GUI

--- src/de/fhswf/verwaltung/DialogFahrer.java
+++ src/de/fhswf/verwaltung/DialogFahrer.java
@@ -5,10 +5,11 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
+import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -32,14 +33,14 @@ public class DialogFahrer extends JDialog
 	   private static final long serialVersionUID = 1L;
 
 	   protected JTextField nameFahrer;
-	   protected JComboBox fKlasse;
+	   protected JCheckBox A1, A, B, C1, C, D1, D, BE, C1E, CE, D1E, DE, M, L, TS;
 	   protected JDatePicker fDatum;
 	   
 	   private JButton deleteButton;
 	   private JButton saveButton;
 	   private JButton exitButton;
 	   
-	   private String[] fKlasseStrings= { "A1", "A", "B", "BE", "C1", "C1E", "CE", "D", "M" };
+	   private String[] fKlasseStrings= { "A1", "A", "B", "C1", "C", "D1", "D", "BE", "C1E", "CE", "D1E", "DE", "M", "L", "T/S" };
 	   
 	   private int row = -1;
 	   
@@ -67,16 +68,19 @@ public class DialogFahrer extends JDialog
 
 	      JPanel vertBox = new JPanel();
 	      vertBox.setLayout(new BoxLayout(vertBox, BoxLayout.PAGE_AXIS));
-	            
-	      JLabel label = new JLabel();
-	      label.setPreferredSize(new Dimension(100, 10));
 
+	      JPanel vertBox1 = new JPanel();
+	      vertBox1.setLayout(new BorderLayout());
+	      JPanel vertBox2 = new JPanel();
+	      vertBox2.setLayout(new BorderLayout());
+	      
 	      JPanel horiBox = new JPanel();
 	      
 	      horiBox = new JPanel();
 	      horiBox.setLayout(new BoxLayout(horiBox, BoxLayout.LINE_AXIS));
 	      JLabel nameLabel = new JLabel(" Fahrer Name: ");
-	      nameLabel.setPreferredSize(new Dimension(100, 10));
+	      nameLabel.setPreferredSize(new Dimension(140, 30));
+	      nameLabel.setMaximumSize(new Dimension(140, 30));
 	      horiBox.add(nameLabel);
 	      nameFahrer = new JTextField();
 	      horiBox.add(nameFahrer);
@@ -84,20 +88,110 @@ public class DialogFahrer extends JDialog
 	      
 	      horiBox = new JPanel();
 	      horiBox.setLayout(new BoxLayout(horiBox, BoxLayout.LINE_AXIS));
-	      JLabel ectslabel = new JLabel(" ECTS Punkte: ");
-	      ectslabel.setPreferredSize(new Dimension(100, 10));
-	      horiBox.add(ectslabel);
-	      fKlasse = new JComboBox(fKlasseStrings);
-	      fKlasse.setEditable(false);
-	      fKlasse.setSelectedIndex(0);
-	      fKlasse.addActionListener(this);
-	      horiBox.add(fKlasse);
+	      A1 = new JCheckBox(fKlasseStrings[0] + "  ");
+	      A1.setMnemonic(KeyEvent.VK_A);
+	      A1.setSelected(false);
+	      horiBox.add(A1);
+	      
+	      A = new JCheckBox(fKlasseStrings[1] + "   ");
+	      A.setMnemonic(KeyEvent.VK_A);
+	      A.setSelected(false);
+	      horiBox.add(A);
+	      vertBox1.add(horiBox, BorderLayout.NORTH);
+	      
+	      horiBox = new JPanel();
+	      horiBox.setLayout(new BoxLayout(horiBox, BoxLayout.LINE_AXIS));
+	      B = new JCheckBox(fKlasseStrings[2] + "    ");
+	      B.setMnemonic(KeyEvent.VK_B);
+	      B.setSelected(false);
+	      horiBox.add(B);
+	      
+	      BE = new JCheckBox(fKlasseStrings[7] + "  ");
+	      BE.setMnemonic(KeyEvent.VK_B);
+	      BE.setSelected(false);
+	      horiBox.add(BE);
+	      vertBox1.add(horiBox, BorderLayout.WEST);
+	      
+	      horiBox = new JPanel();
+	      horiBox.setLayout(new BoxLayout(horiBox, BoxLayout.LINE_AXIS));
+	      C1 = new JCheckBox(fKlasseStrings[3] + "  ");
+	      C1.setMnemonic(KeyEvent.VK_C);
+	      C1.setSelected(false);
+	      horiBox.add(C1);
+
+	      C1E = new JCheckBox(fKlasseStrings[8]);
+	      C1E.setMnemonic(KeyEvent.VK_C);
+	      C1E.setSelected(false);
+	      horiBox.add(C1E);
+
+	      C = new JCheckBox(fKlasseStrings[4] + "    ");
+	      C.setMnemonic(KeyEvent.VK_C);
+	      C.setSelected(false);
+	      horiBox.add(C);
+
+	      CE = new JCheckBox(fKlasseStrings[9] + "  ");
+	      CE.setMnemonic(KeyEvent.VK_C);
+	      CE.setSelected(false);
+	      horiBox.add(CE);
+	      vertBox1.add(horiBox, BorderLayout.SOUTH);
+	      
+	      horiBox = new JPanel();
+	      horiBox.setLayout(new BoxLayout(horiBox, BoxLayout.LINE_AXIS));
+	      D1 = new JCheckBox(fKlasseStrings[5] + "  ");
+	      D1.setMnemonic(KeyEvent.VK_D);
+	      D1.setSelected(false);
+	      horiBox.add(D1);
+
+	      D1E = new JCheckBox(fKlasseStrings[10]);
+	      D1E.setMnemonic(KeyEvent.VK_D);
+	      D1E.setSelected(false);
+	      horiBox.add(D1E);
+
+	      D = new JCheckBox(fKlasseStrings[6] + "    ");
+	      D.setMnemonic(KeyEvent.VK_D);
+	      D.setSelected(false);
+	      horiBox.add(D);
+
+	      DE = new JCheckBox(fKlasseStrings[11] + "  ");
+	      DE.setMnemonic(KeyEvent.VK_D);
+	      DE.setSelected(false);
+	      horiBox.add(DE);
+	      vertBox2.add(vertBox1, BorderLayout.NORTH);
+	      vertBox2.add(horiBox, BorderLayout.WEST);
+	      
+	      horiBox = new JPanel();
+	      horiBox.setLayout(new BoxLayout(horiBox, BoxLayout.LINE_AXIS));
+	      M = new JCheckBox(fKlasseStrings[12] + "    ");
+	      M.setMnemonic(KeyEvent.VK_M);
+	      M.setSelected(false);
+	      horiBox.add(M);
+
+	      L = new JCheckBox(fKlasseStrings[13] + "    ");
+	      L.setMnemonic(KeyEvent.VK_L);
+	      L.setSelected(false);
+	      horiBox.add(L);
+
+	      TS = new JCheckBox(fKlasseStrings[14]);
+	      TS.setMnemonic(KeyEvent.VK_T);
+	      TS.setSelected(false);
+	      horiBox.add(TS);
+	      vertBox2.add(horiBox, BorderLayout.SOUTH);
+	      
+	      horiBox = new JPanel();
+	      horiBox.setLayout(new BorderLayout());
+	      JLabel a1Label = new JLabel(" Fuehrerscheinklassen: ");
+	      a1Label.setPreferredSize(new Dimension(140, 30));
+	      a1Label.setMaximumSize(new Dimension(140, 30));
+	      horiBox.add(a1Label, BorderLayout.WEST);
+//	      horiBox.add(vertBox1, BorderLayout.WEST);
+	      horiBox.add(vertBox2, BorderLayout.CENTER);
 	      vertBox.add(horiBox);
 
 	      horiBox = new JPanel();
 	      horiBox.setLayout(new BoxLayout(horiBox, BoxLayout.LINE_AXIS));
 	      JLabel fSeitLabel = new JLabel(" Fuehrerschein Seit: ");
-	      fSeitLabel.setPreferredSize(new Dimension(100, 10));
+	      fSeitLabel.setPreferredSize(new Dimension(140, 30));
+	      fSeitLabel.setMaximumSize(new Dimension(140, 30));
 	      horiBox.add(fSeitLabel);
 	      fDatum = new JDatePicker();
 	      horiBox.add(fDatum);
@@ -155,7 +249,7 @@ public class DialogFahrer extends JDialog
 	   public void loadFahrer(Fahrer fahrer)
 	   {
 	      nameFahrer.setText(fahrer.getName());
-	      fKlasse.setSelectedIndex(fahrer.getFueKlasse());
+//	      fKlasse.setSelectedIndex(fahrer.getFueKlasse());
 	      fDatum.setSelectedDate(fahrer.getFueSeit());
 	   }
 
